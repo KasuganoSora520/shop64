9212547443271458b8f8d32918232a3cf8355392
sister <m18863010215@163.com>
2019-02-17 19:09:09 +0800

    初写登录页面

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 开始了 -->
+    div....
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '../components/login.vue'
+// import login from "@/components/login.vue"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home'
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'home'
+		},
+		{
+			name: 'login',
+			path: '/login',
+			component: Login
+		}
+	]
 })
