973fd54be2d37867da764996e56584f741613f63
sister <m18863010215@163.com>
2019-02-17 19:22:28 +0800

    引入登录组组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,29 @@
 <template>
-  <div id="app">
-    <!-- 开始了 -->
-    div....
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<el-form label-position="top" label-width="80px" :model="formData">
+			<el-form-item label="用户名">
+				<el-input v-model="formData.username"></el-input>
+			</el-form-item>
+			<el-form-item label="密码">
+				<el-input v-model="formData.password"></el-input>
+			</el-form-item>
+			<el-button type="danger" round>登录</el-button>
+		</el-form>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	export default {
+		name: 'App',
+		data () {
+			return {
+				formData: {
+					username:'',
+					password:''
+				}
+			}
+		}
+	}
 </script>
 
 <style>
